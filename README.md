2fd76b1f5648915ee99915184ccd5a3f43b43620
Supriya090 <khadkasupriya76@gmail.com>
2023-10-04 16:31:06 +0545

    Application before database complete

---
+++ README.md
@@ -0,0 +1,37 @@
+## Teaching Guidelines
+
+### Phase 1
+
+- Check Installations
+- See the opening screen
+- Install Nodemon
+- A tour of files
+- EJS a template engine
+- Routing
+	- HTTP method
+	- HTTP Request
+	- HTTP Response
+	- HTTP Status codes
+
+- Add gitignore
+- Push to repo
+
+**Explain what all are you going to talk about**
+- Loading from local document
+- Making a dynamic page, getting data from user
+- Pushing that data to database, loading data from database (Sarayu's part)
+
+---
+
+### Phase 2
+
+- Install and explain body parser
+- Make the books path
+- Explain why the books path is necessary
+	- Explain the alternatives
+- Add and show books
+- Post books
+- Remove, edit (Ask fellows to do edit post)
+- Show why database is necessary
+
+---
--- routes/books.js
+++ routes/books.js
@@ -1,11 +1,38 @@
 const { Router } = require('express');
 var express = require('express');
+var books = require('../resources/books');
 var router = express.Router();
 
 router.get('/add', function(req, res, next){
     res.render('addBooks', {
         title: 'Add book',
     });
+})
+
+router.post('/save', function(req, res){
+    books.push({...req.body, _id: `00${books.length + 1}`});
+    res.redirect('/');
+  })
+  
+router.get('/remove/:_id', function(req,res){
+books.splice(req.params._id, 1);
+res.redirect('/');
+})
+
+router.get('/edit/:_id', function(req,res){
+console.log(req.params._id);
+const book = books.find((book)=>book._id === req.params._id);
+res.render('editBooks',{
+    title:"Edit Book",
+    book
+})
+})
+
+// Ask fellows to do this
+router.post('/edit/:_id', function(req,res){
+let currIndex = books.findIndex(book => book._id === req.params._id);
+books.splice(currIndex, 1, {...req.body, _id: req.params._id});
+res.redirect('/');
 })
 
 module.exports = router;
